ea8f35f1d1f0c38ec659652e5b9f49f924128674
Marceline the Vampire Queen <marceline@example.com>
2019-03-25 20:23:18 -0400

    calculate code to answer homework questions

--- lib/proto/MPS7/parser.rb
+++ lib/proto/MPS7/parser.rb
@@ -25,17 +25,52 @@ module Parser
 
       data = contents.bytes.slice!(9..-1).pack("C*")
       index = 0
+      calculations = { :debit => 0,
+                       :credit => 0,
+                       :started_count => 0,
+                       :ended_count => 0,
+                       '2456938384156277127' => 0 }
 
       while data[index].present?
-        # byebug
-        if data[index].unpack("C")[0]  == 0 || data[index].unpack("C")[0] == 1
-          puts data[index..index + 21].unpack("CL>Q>G")
+        # debit
+        if data[index].unpack("C")[0]  == 0
+          record = data[index..index + 21].unpack("CL>Q>G")
+          puts record
+          # total amount in dollars of debits
+          calculations[:debit] += record[3].to_f
+          # for user ID 2456938384156277127,
+          if record[2] == 2456938384156277127
+            # sum the dollars of debits from the user's total balance
+            calculations['2456938384156277127'] += record[3].to_f
+          end
           index += 21
-        else
+        # credit
+        elsif data[index].unpack("C")[0] == 1
+          record = data[index..index + 21].unpack("CL>Q>G")
+          puts record
+          # total amount in dollars of credits
+          calculations[:credit] += record[3].to_f
+          # for user ID 2456938384156277127,
+          if record[2] == 2456938384156277127
+            # subtract the dollars of credits from the user's total balance
+            calculations['2456938384156277127'] -= record[3].to_f
+          end
+          index += 21
+          # StartAutopay
+        elsif data[index].unpack("C")[0] == 2
           puts data[index..index + 13].unpack("CL>Q>")
+          # increase the total count of how many times Autopay has been started
+          calculations[:started_count] += 1
+          index += 13
+          # EndAutopay
+        elsif data[index].unpack("C")[0] == 3
+          puts data[index..index + 13].unpack("CL>Q>")
+          # increase the total count of how many times Autopay has been started
+          calculations[:ended_count] += 1
           index += 13
         end
       end
+      calculations
     end
   end
 end
