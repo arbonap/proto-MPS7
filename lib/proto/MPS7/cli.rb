56555189a5a0eafdaa22669f4880c035ffd8b037
Patricia Arbona <arbonap@gmail.com>
2019-04-10 01:03:35 -0400

    update readme set up instructions

--- lib/proto/MPS7/cli.rb
+++ lib/proto/MPS7/cli.rb
@@ -4,14 +4,7 @@ require_relative "parser"
   class Cli
     extend Parser
 
-    # byebug
     puts Parser::BinaryProtocol.ascii
-    # Parser::BinaryProtocol.welcome
     @protocol = Parser::BinaryProtocol.new(ARGV)
     @protocol.open
-    #
-    # @normalization = Parser::Normalization.new(ARGV)
-    # @normalization.truncate if File.open('normalized_data.csv', "a+").present?
-    # @normalization.scrub
-    # @normalization.normalize
   end
