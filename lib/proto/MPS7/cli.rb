137262e769cbd03d37c38e5c6c759eedb9fbfe25
Patricia Arbona <arbonap@gmail.com>
2019-04-15 23:10:41 -0400

    test

--- lib/proto/MPS7/cli.rb
+++ lib/proto/MPS7/cli.rb
@@ -4,6 +4,7 @@ require_relative "parser"
     extend Parser
 
     puts Parser::BinaryProtocol.ascii
+    `proto_MPS7 txnlog.dat`
     @protocol = Parser::BinaryProtocol.new(ARGV)
     calculations = @protocol.parse
     puts Parser::BinaryProtocol.answers(calculations)
